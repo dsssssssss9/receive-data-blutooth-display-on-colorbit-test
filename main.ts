623293442bff117ad13db2bf45fe299d36edb1f3
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-27 19:30:00 +0100

    add dice icons to microbit display

--- main.ts
+++ main.ts
@@ -1,12 +1,71 @@
 radio.onReceivedNumber(function (receivedNumber) {
     Dice = receivedNumber
-    basic.showNumber(Dice)
+    if (Dice == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Dice == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Dice == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Dice == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Dice == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Dice == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
 let Dice = 0
 radio.setGroup(42)
 let colorbit_51bit = colorbit.initColorBit(DigitalPin.P2, BitColorMode.RGB)
 colorbit_51bit.setBrightness(16)
+colorbit_51bit.show()
+colorbit_51bit.showColorIcon(ColorIcon.Yes, colorbit.colors(BitColors.Red))
+basic.pause(1000)
 colorbit_51bit.clear()
 basic.forever(function () {
-	
+    colorbit_51bit.showIntColor(Dice, colorbit.colors(BitColors.Green))
 })
